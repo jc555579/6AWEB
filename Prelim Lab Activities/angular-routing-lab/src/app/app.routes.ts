0cab51a12a09fe0466344d678d99ffc22684d5ed
Joemar Catipon <joemarcatipon27@gmail.com>
2025-12-04 08:34:22 +0800

    Modified Angular routing including deployment

--- Prelim Lab Activities/angular-routing-lab/src/app/app.routes.ts
+++ Prelim Lab Activities/angular-routing-lab/src/app/app.routes.ts
@@ -2,11 +2,17 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { DataBinding } from './data-binding/data-binding';
 import { Directives } from './directives/directives';
+import { Pagenotfound } from './pagenotfound/pagenotfound';
+import { Bootstrap } from './bootstrap/bootstrap';
+import { Tailwind } from './tailwind/tailwind';
 
 
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'directives', component: Directives},
-  { path: 'data-binding', component: DataBinding }
+  { path: 'data-binding', component: DataBinding },
+  { path: 'pagenotfound', component: Pagenotfound },
+  { path: 'bootstrap', component: Bootstrap },
+  { path: 'tailwind', component: Tailwind }
 ];
